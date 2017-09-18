78b2fbf60f858de3a4edac9eb1ad6193b53c66a6
Martin Dieblich <martin.dieblich@gmx.de>
2017-09-18 18:29:50 +0200

    Hauptfenster an richtige Stelle verschoben

--- src/com/dieblich/handball/habaDiggiGui/ChooseGameWindow.java
+++ src/com/dieblich/handball/habaDiggiGui/ChooseGameWindow.java
@@ -1,8 +1,6 @@
-package sample;
+package com.dieblich.handball.habaDiggiGui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
