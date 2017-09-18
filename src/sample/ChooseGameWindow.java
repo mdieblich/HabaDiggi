83c67ed70b26f581c1e3cd74db4dfe9e494c2f55
Martin Dieblich <martin.dieblich@gmx.de>
2017-09-18 18:25:09 +0200

    Projekt angelegt

---
+++ src/sample/ChooseGameWindow.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ChooseGameWindow extends Application {
+
+    private Button startNewGameButton = new Button("Neues Spiel erstellen");
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("HabaDiggi: Spiel auswählen");
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(startNewGameButton);
+        Scene mainScene  = new Scene(borderPane, 300, 275);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
